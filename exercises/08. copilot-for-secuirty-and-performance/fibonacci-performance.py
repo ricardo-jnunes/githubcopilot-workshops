fb1833c9e9155929509d9c9bafb90ace0711fa70
Ricardo J. Nunes <jnunes.ricardo@gmail.com>
2026-08-27 11:50:36 -0300

    docs: add exercises for creating functions, unit-test boilerplates and  vulnerabilities and performance improvement with GitHub Copilot

---
+++ exercises/08. copilot-for-secuirty-and-performance/fibonacci-performance.py
@@ -0,0 +1,4 @@
+# create a fibonacci function without memoization.
+
+
+# create a fibonacci function that uses recursion and memoization to improve performance, with memoization implemented using a dictionary to store previously computed values. 
---
+++ exercises/08. copilot-for-secuirty-and-performance/identify-security-vulnerabilities.py
@@ -0,0 +1,5 @@
+#import sqlite
+#harcoded database password
+#create function to register user and password in database using f-string in SQL
+#create function to login user and password usinf f-string in SQL
+#create main function to demo register and login functions
